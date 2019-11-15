7ebcf47054be286cf31b75858d933f9b4db53cc7
Grace Petrosini <gpetrosi@nd.edu>
2019-11-15 12:20:58 -0500

    #get the script to go back to readline() after lower or higher also figure out how to get scoreSum to work again

--- BiocompEx08.R
+++ BiocompEx08.R
@@ -4,6 +4,8 @@ scoreUW<-score[which(score[,2]=="UW"),]
 scoreMSU<-score[which(score[,2]=="MSU"),]
 
 #how did i get scoreSum to do what i wanted before??
+scoreSumUW=
+scoreSumMSU=
 
 # Generate some data
 x<-score$time
@@ -11,16 +13,14 @@ y1<-scoreUW$scoreSum
 y2<-scoreMSU$scoreSum
 plot(x, y1, type="l", pch=19, col="red", xlab="Time", ylab="Score")
 # Add a line
-lines(x, y2, pch=18, col="green", type="l", lty=2)
+lines(x, y2, pch=18, col="green", type="l", lty=1)
 # Add a legend
 legend(1, 95, legend=c("UW", "MSU"),
-       col=c("red", "green"), lty=1:2, cex=0.8)
-
-#plot(1,type='n',xlim=c(1,40),ylim=c(0,100),xlab='Time', ylab='Score')
+       col=c("red", "green"), lty=solid, cex=0.8)
 
 #Guess My Number game
 n<-sample(1:100, 1, replace = TRUE)
-print("Guess:")
+x<- readline(prompt = "Guess: ")
 if (x>n){
   print("Lower")
 }else if (x<n){
@@ -28,3 +28,4 @@ if (x>n){
 }else{
   print("Correct!")
 }
+#get the script to go back to readline() after "lower" or "higher"
