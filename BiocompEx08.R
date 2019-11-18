6b9c704c516a7637a129df8a51fed3ae1e1e74d6
Grace Petrosini <gpetrosi@nd.edu>
2019-11-18 11:15:03 -0500

    trying ggplot instead of plot() and lines()

--- BiocompEx08.R
+++ BiocompEx08.R
@@ -1,19 +1,24 @@
 #Plotting
-score<-read.table("./Biocomp_exercise08/UWvMSU_1-22-13.txt", header = TRUE)
+library(ggplot2)
+score<-read.table("./Biocomp_exercise08/UWvMSU_1-22-13.txt", header = TRUE, stringsAsFactors = FALSE)
+head(score)
+a=ggplot(data=score, mapping=aes(x=time, y=score))
+a+geom_line(aes(color=team))+xlab("Time (min)")+ylab("Points Scored")+
+  theme_bw()
+
+
 scoreUW<-score[which(score[,2]=="UW"),]
 scoreMSU<-score[which(score[,2]=="MSU"),]
 
 #how did i get scoreSum to do what i wanted before??
-scoreSumUW=
-scoreSumMSU=
+scoreSumUW=cumsum(scoreUW[,3])
+scoreSumMSU=cumsum(scoreMSU[,3])
 
 # Generate some data
-x<-score$time
-y1<-scoreUW$scoreSum
-y2<-scoreMSU$scoreSum
-plot(x, y1, type="l", pch=19, col="red", xlab="Time", ylab="Score")
+timeGame<-score$time
+plot(timeGame, scoreSumUW, type="l", pch=19, col="red", xlab="Time", ylab="Score")
 # Add a line
-lines(x, y2, pch=18, col="green", type="l", lty=1)
+lines(timeGame, scoreSumMSU, pch=18, col="green", type="l", lty=1)
 # Add a legend
 legend(1, 95, legend=c("UW", "MSU"),
        col=c("red", "green"), lty=solid, cex=0.8)
