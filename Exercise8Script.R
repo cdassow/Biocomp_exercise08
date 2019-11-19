4db7ee50566147f0159a622ecb3492ac96affa59
Lindsey Mortensen <lmortens@nd.edu>
2019-11-19 14:00:28 -0500

    I think this is the correct way for Q2...

--- Exercise8Script.R
+++ Exercise8Script.R
@@ -1,7 +1,9 @@
 setwd("~/Desktop/biocomp-shell/Biocomp_exercise08/")
+
 install.packages("ggplot2")
 library(ggplot2)
 
+#Question 1
 UWMSU <- read.table("UWvMSU_1-22-13.txt", header=TRUE)
 UWMSUdf <- as.data.frame(UWMSU)
 
@@ -13,7 +15,6 @@ for (i in 1:length(UWscorevec)){
   UWcuml[i] <- sum(UWscorevec[1:i]) 
 }
 UW["cumlscore"] <- UWcuml
-
 
 #Only MSU data, repeat process above for MSU scores
 MSU <- UWMSU[UWMSU$team=="MSU",]
@@ -27,9 +28,24 @@ MSU["cumlscore"] <- MSUcuml
 #Merge two dataframes to one
 newdata <- rbind(MSU,UW)
 
-#Plot data
+#Plot data (remember to install ggplot)
 ex8graph <- ggplot(data=newdata, mapping=aes(x=time, y=cumlscore))
 ex8graph+geom_line(aes(color=team), size=2)+xlab("Time (minutes)")+ylab("Points")+theme_classic()+
   scale_color_manual(values=c("palegreen4","red"))
 
+#Question 2
+#Write a game called “guess my number”. 
+#Random number selected by computer
+
+print("Guess a number between 1 and 100 until you get it right")
+randomnumber <- sample(1:100,1)
+for (i in 1:10){
+  input <- as.numeric(readline())
+  if (input>randomnumber){
+    print("Lower")}
+  else if (input<randomnumber){
+    print("Higher")}
+  else{
+    print("Correct!")}
+}
 
