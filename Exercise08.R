c78f31f67284662593a6a21b4274b3161bdea4ad
angeloliu1200 <angelo.liu1200@gmail.com>
2019-11-19 22:02:18 -0500

    finish

--- Exercise08.R
+++ Exercise08.R
@@ -1,3 +1,12 @@
+#Problem 1
+sports=read.table("UWvMSU_1-22-13.txt", sep=" ", header=TRUE)
+UW = sports[sports$team=="UW",]
+UW$UW_Cum_Score = cumsum(UW$score)
+MSU = sports[sports$team=="MSU",]
+MSU$MSU_Cum_Score = cumsum(MSU$score)
+plot(UW$time,UW$UW_Cum_Score, type='l',xlab="Time",ylab="Score")
+lines(MSU$time,MSU$MSU_Cum_Score)
+
 #Problem 2
 game = function(){
   print("I'm thinking of a number 1-100...")
