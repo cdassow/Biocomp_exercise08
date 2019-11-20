ee52f489257dd14f650e9971187f5fdce0572226
Alice Bruemmer <alicebruemmer1@gmail.com>
2019-11-20 15:45:13 -0500

    completed part 1

--- exercise8.R
+++ exercise8.R
@@ -2,32 +2,33 @@
 
 #1. generate a graph from the UW vs. MSU game on 1/22/13
 
-#generate a datagrame with a cumulative score for each team when they score
+#generate a dataframe with a cumulative score for each team when they score
 gameData <- read.delim("UWvMSU_1-22-13.txt")
 
-UWscore <- 0
+
+UWScore <- 0
 MSUScore <- 0
 
-for(i in 1:nrow(gameData)){
+for(i in 1:50){
   if(gameData$team[i]=="UW"){
-    UWScore <- UWScore + gameData$score[i]
+   print(gameData$score[i])
+   
   }else{
     MSUScore <- MSUScore + gameData$score[i]
   }
 }
 
 
-UWScore <- 0
-MSUScore <- 0
 
-for(i in 1:nrow(gameData)){
-  if(gameData$team[i]=="UW"){
-    UWScore <- cumsum(gameData$score[1:50])
-  }else{
-    MSUScore <- cumsum(gameData$score[1:50])
-  }
-}
+UW <- data.frame(time=UWTime, score=UWScore)
+MSU <- data.frame(time=MSUTime, MSUscore=MSUScore)
+
+time <- gameData$time
+timeFrame <- data.frame(x=time)
 
 
+
+plot(UWTime,UWScore,type='l')
+lines(MSUTime,MSUScore)
 
 
