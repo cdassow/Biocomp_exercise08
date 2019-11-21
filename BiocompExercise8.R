b3a6b1d75f0f5f38415a650544d995bdebaf59a6
Sydney Louden <slouden@nd.edu>
2019-11-20 22:41:00 -0500

    Part 2 finished

--- BiocompExercise8.R
+++ BiocompExercise8.R
@@ -1,51 +1,67 @@
 #Part 1
 #Load game data
-score<-read.table("~/Biocomp/Biocomp_exercise08/UWvMSU_1-22-13.txt")
-nrow(score)
-#Create data frame subset of just UW data
-UW_data<-as.data.frame(score[score$V2=="UW",])
-
-#Create data frame subset of just MSU data
-MSU_data<-as.data.frame(score[score$V2=="MSU",])
-
-UWscore<-matrix(nrow=50, ncol=2)
-MSUscore<-matrix(nrow=50, ncol=2)
-UWtotal<-0
-MSUtotal<-0
-for(i in 2:nrow(score)){
-  if(score[score$V2="UW"]){
-    UWsum<-sum(UWtotal+score[[i],score$V3])
-    UWscore[[i],]<-UWsum
-    MSUsum<-MSUtotal
-    MSUscore[i,]<-MSUsum
+data<-read.table("~/Biocomp/Biocomp_exercise08/UWvMSU_1-22-13.txt", header=TRUE)
+head(score)
+sumFemale<-0
+sumMale<-0
+for(i in 1:nrow(wages)){
+  if(wages[i,]$gender=="female"){
+    sumFemale<-sumFemale+wages[i,]$wage  
   }else{
-    UWsum<-UWtotal
-    MSUsum<-sum(MSUtotal+score[i,score$V3])
-    MSUscore[i,]<-MSUsum
+    sumMale<-sumMale+wages[i,]$wage
+  } 
+}
+sumFemale
+sumMale
+
+for(i in 1:nrow(data)){
+  if(data[i,]$team=="UW"){
+    UWsum=UWsum+data[i,]$score
+    UWscore[i,]=UWsum
   }
 }
+UWscore
+ # }else{
+  #  MSUsum=MSUsum+score[i,]$V3
+   # MSUscore[i,]=MSUsum
+  #}
+#}
 
-
+UW<-score[score$V2=="UW",]
+UW
+for(i in 1:nrow(UW)){
+  UWsum<-UWsum+UW[i,]$V3
+  UWscore[i,]<-UWsum
+}
+UWscore
+UWscore<-matrix(nrow=50, ncol=2)
+UWscore[,1]<-(1:50)
+UWscore
+MSUscore<-matrix(nrow=50, ncol=2)
+UWscore[1,2]<-3
+head(UWscore)
+UWsum<-0
+MSUsum<-0
 
 #Part 2
-#Generate a random number a
-
+#Generate a random number stored as a
 a<-sample(1:100,1)
-
-#Function
-
-
-answer<-readline(prompt="Guess ")
-if(a>answer){
-   print("lower")
-}else{
-  if(a<answer){
-    print("higher")
-  }else{
-    if(a==answer){
-      print("correct")
-     }
+#The function NumberGuess that runs 10 times
+NumberGuess<-function(){
+  for(i in 1:10){
+    guess<-readline(prompt="Guess: ")
+    if(a<guess){
+      print("lower")
+    }else{
+      if(a>guess){
+        print("higher")
+      }else{
+        if(a==guess){
+          print("correct")
+        }
+      }
+    }
   }
 }
-
-a
+#Run the function
+NumberGuess()
