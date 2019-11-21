529fdfb38437a1bbc362c9d3c08dcf69d0ab76ba
Sydney Louden <slouden@nd.edu>
2019-11-21 11:13:01 -0500

    Part 1 finished

--- BiocompExercise8.R
+++ BiocompExercise8.R
@@ -1,47 +1,14 @@
 #Part 1
 #Load game data
 data<-read.table("~/Biocomp/Biocomp_exercise08/UWvMSU_1-22-13.txt", header=TRUE)
-head(score)
-sumFemale<-0
-sumMale<-0
-for(i in 1:nrow(wages)){
-  if(wages[i,]$gender=="female"){
-    sumFemale<-sumFemale+wages[i,]$wage  
-  }else{
-    sumMale<-sumMale+wages[i,]$wage
-  } 
-}
-sumFemale
-sumMale
-
-for(i in 1:nrow(data)){
-  if(data[i,]$team=="UW"){
-    UWsum=UWsum+data[i,]$score
-    UWscore[i,]=UWsum
-  }
-}
-UWscore
- # }else{
-  #  MSUsum=MSUsum+score[i,]$V3
-   # MSUscore[i,]=MSUsum
-  #}
-#}
-
-UW<-score[score$V2=="UW",]
-UW
-for(i in 1:nrow(UW)){
-  UWsum<-UWsum+UW[i,]$V3
-  UWscore[i,]<-UWsum
-}
-UWscore
-UWscore<-matrix(nrow=50, ncol=2)
-UWscore[,1]<-(1:50)
-UWscore
-MSUscore<-matrix(nrow=50, ncol=2)
-UWscore[1,2]<-3
-head(UWscore)
-UWsum<-0
-MSUsum<-0
+#Separate data by team
+UWdata<-data[data$team=="UW",]
+MSUdata<-data[data$team=="MSU",]
+#Create a column of cumulative scores over time for each team
+UWdata$UWtotals<-cumsum(UWdata$score)
+MSUdata$MSUtotals<-cumsum(MSUdata$score)
+#Plot the data
+plot(UWdata$time, UWdata$UWtotals, type="l", xlab="Time (min)", ylab="Cumulative score", col="red")+lines(MSUdata$time, MSUdata$MSUtotals, col="blue")
 
 #Part 2
 #Generate a random number stored as a
