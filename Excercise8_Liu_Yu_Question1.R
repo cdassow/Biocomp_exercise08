dfc8445c9cc4454a818976c03a62c2e7b6c266ed
刘国强 <orson@liuguoqangdeMBP.mshome.net>
2019-11-21 16:51:53 -0500

    complete the 2nd question, work together with Sihan Yu

--- Excercise8_Liu_Yu_Question1.R
+++ Excercise8_Liu_Yu_Question1.R
@@ -1,7 +1,7 @@
 # Question 1
 
 # set path to the folder in local
-setwd('/Volumes/GoogleDrive/我的云端硬盘/ND2019Fall/Intro\ Biocomputing/Excercise\ 8/Biocomp_exercise08')
+setwd('/Volumes/GoogleDrive/My\ Drive/ND2019Fall/Intro\ Biocomputing/Excercise\ 8/Biocomp_exercise08')
 
 # import the score-by-score form
 score = read.table('UWvMSU_1-22-13.txt',header = T)
@@ -9,22 +9,23 @@ score = read.table('UWvMSU_1-22-13.txt',header = T)
 # run the loops to calculate the cumulative score of each team in each time point
 nr = nrow(score)
 time = score$time
-uw.score = c()
+
+uw.score = c() # create empty vector to store the cumulative scores
 msu.score = c()
 for (i in c(1:nr)){
-  score.tmp = score[1:i,]
-  uw.score.tmp = sum(score.tmp[which(score.tmp$team=="UW"),"score"]) 
+  score.tmp = score[1:i,] # disset temporary scores
+  uw.score.tmp = sum(score.tmp[which(score.tmp$team=="UW"),"score"]) # calculate cumulative scores
   uw.score = c(uw.score,uw.score.tmp)
   msu.score.tmp = sum(score.tmp[which(score.tmp$team=="MSU"),"score"]) 
   msu.score = c(msu.score,msu.score.tmp)
 }
 
+# plot the scores of two teams
 plot(time,uw.score,type='l',col = 'red', 
      xlab = 'Time (min)', ylab = 'Score', main = 'UW vs MSU')
 lines(time,msu.score, col = 'blue')
+# add legends
 legend("topleft", legend = c('UW Score', 'MSU Score'),
        col = c('red', 'blue'), lty = 1, cex = 0.8)
-
-
 
 
